1f191f38d945772b780ac9d30605cf0a324d1d96
Daniya Yadav <daniyayadav@Daniyas-MacBook-Air.local>
2026-02-14 20:50:00 +0530

    commit1

---
+++ data.js
@@ -0,0 +1,42 @@
+const products = [
+  {
+    id: 1,
+    name: "Velvet Royal Gown",
+    price: 18999,
+    category: "Women",
+    image: "velvet royal gown.webp",
+    description: "Premium velvet evening gown for luxury occasions."
+  },
+  {
+    id: 2,
+    name: "Italian Tailored Suit",
+    price: 24999,
+    category: "Men",
+    image: "https://images.unsplash.com/photo-1593032465175-481ac7f401a0",
+    description: "Handcrafted luxury Italian suit."
+  },
+  {
+    id: 3,
+    name: "Designer Silk Saree",
+    price: 21999,
+    category: "Women",
+    image: "designer silk saree.jpg",
+    description: "Golden embroidered silk saree."
+  },
+  {
+    id: 4,
+    name: "Classic Trench Coat",
+    price: 15999,
+    category: "Unisex",
+    image: "trench coat.avif",
+    description: "Luxury wool blend trench coat."
+  },
+  {
+    id: 5,
+    name: "Limited Leather Jacket",
+    price: 29999,
+    category: "Men",
+    image: "https://images.unsplash.com/photo-1593030761757-71fae45fa0e7",
+    description: "Premium limited edition leather jacket."
+  }
+];
